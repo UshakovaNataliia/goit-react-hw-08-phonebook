e070437cac8d1917d1cea19a6619520abf58d0fe
UshakovaNataliia <65922563+UshakovaNataliia@users.noreply.github.com>
2020-12-17 19:54:08 +0200

    done

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import styles from '../App/App.module.css';
 
 export const Error = () => {
-  return <div className={styles.error}>Invalod login or password</div>;
+  return <div className={styles.error}>Invalid login or password</div>;
 };
